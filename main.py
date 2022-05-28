8bf702522022189e96530f3a623daa5bd9e75a45
echometerain <echometer@disroot.org>
2022-05-28 13:28:52 -0400

    move movement code from main to player

--- main.py
+++ main.py
@@ -15,31 +15,22 @@ import sys
 black = c.image("black")
 mask = c.image("mask")
 ground.make()
-move_count = 0
-moving = False
 
 
 def next():  # updates frame
     pg.display.update()
-    pg.event.clear()
     c.clock.tick(100)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             sys.exit()
             exit(0)
-
-
-def in_radius(x, y, r):
-    return x*x + y*y <= r*r
+    pg.event.clear()
 
 
 def keys():
-    global move_count
-    global moving
     x = 0
     y = 0
-    move_vector = [0, 0]
     keys = pg.key.get_pressed()  # x is always inverted for some reason
     if keys[pg.K_w]:
         y += 1
@@ -49,23 +40,7 @@ def keys():
         x -= 1
     if keys[pg.K_d]:
         x += 1
-    if x == 0 and y == 0:
-        moving = False
-        c.pl_state[2] = 3
-        return
-    elif moving == False:
-        moving = True
-        move_count = 10
-        player.upd()
-    move_vector, c.pl_state = c.move_anim(
-        x, y, c.SPEED, c.DIAG_SPEED, c.pl_state)
-    if in_radius(c.offset[0]+move_vector[0], c.offset[1]-move_vector[1], c.OFFSET_RAD):
-        c.offset[0] += move_vector[0]
-        c.offset[1] -= move_vector[1]
-        player.pl.re_position()
-    else:
-        c.pos[0] += move_vector[0]
-        c.pos[1] += move_vector[1]
+    player.pl.move(x, y)
 
 
 def draw():
@@ -76,10 +51,6 @@ def draw():
 
 
 while True:
-    move_count += 1
-    if move_count >= 10:
-        move_count = 0
-        player.upd()
     keys()
     ground.move()
     draw()
--- player.py
+++ player.py
@@ -12,6 +12,9 @@ def get_sprites():
 
 
 class Player(pg.sprite.Sprite):
+    moving = False
+    move_count = 0
+
     def __init__(self):
         super().__init__()
         self.spritesheet = c.image("player")
@@ -36,15 +39,39 @@ class Player(pg.sprite.Sprite):
         self.rect = self.image.get_rect(
             center=(c.WIDTH//2+c.offset[0], c.HEIGHT//2+c.offset[1]))
 
+    def move(self, x, y):
+        move_vector = [0, 0]
+        self.move_count += 1
+        if self.move_count >= 10:
+            self.move_count = 0
+            self.update()
+        if x == 0 and y == 0:
+            self.moving = False
+            c.pl_state[2] = 3
+            return
+        elif self.moving == False:
+            self.moving = True
+            self.move_count = 10
+            self.update()
+        move_vector, c.pl_state = c.move_anim(
+            x, y, c.SPEED, c.DIAG_SPEED, c.pl_state)
+        if in_radius(c.offset[0]+move_vector[0], c.offset[1]-move_vector[1], c.OFFSET_RAD):
+            c.offset[0] += move_vector[0]
+            c.offset[1] -= move_vector[1]
+            self.re_position()
+        else:
+            c.pos[0] += move_vector[0]
+            c.pos[1] += move_vector[1]
+
 
 pl = Player()
 group = pg.sprite.Group()
 group.add(pl)
 
 
-def upd():
-    pl.update()
-
-
 def draw():
     group.draw(c.screen)
+
+
+def in_radius(x, y, r):
+    return x*x + y*y <= r*r
